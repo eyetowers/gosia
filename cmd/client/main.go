63c35b69df39596938ba331a18301c841f9643a7
Ondrej Sery <ondrej.sery@eyetowers.cz>
2025-06-03 13:52:49 +0200

    Implement client helper.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	"github.com/eyetowers/gosia/pkg/sia"
 )
@@ -10,9 +12,19 @@ func main() {
 	if len(os.Args) < 2 {
 		panic("Missing the target address argument.")
 	}
-	i := sia.AuthCode("1120")
-	m := sia.DCS("BA", sia.Named(1, "Partition 1"), sia.Named(2, "Zone 2"), sia.Empty())
-	err := sia.Send(os.Args[1], 1, i, m)
+
+	client, err := sia.New(os.Args[1], sia.AuthCode("1120"), 10*time.Second, func(err error) {
+		fmt.Printf("Ping error: %s\n", err)
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	time.Sleep(time.Minute)
+
+	msg := sia.DCS("BA", sia.Named(1, "Partition 1"), sia.Named(2, "Zone 2"), sia.Empty())
+	err = client.Send(msg)
 	if err != nil {
 		panic(err)
 	}
--- pkg/sia/client.go
+++ pkg/sia/client.go
@@ -2,26 +2,123 @@ package sia
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"net"
+	"sync"
+	"time"
 )
 
-func Send(server string, sequence uint16, identity Identity, message Message) error {
+const (
+	maxSequence = 9999
+)
+
+type PingError func(err error)
+
+type Client struct {
+	server   string
+	identity Identity
+
+	ctx     context.Context
+	stop    context.CancelFunc
+	workers sync.WaitGroup
+
+	mu       sync.Mutex
+	sequence uint16
+}
+
+func New(server string, identity Identity, pingPeriod time.Duration, pingError PingError) (*Client, error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	c := &Client{
+		server:   server,
+		identity: identity,
+		ctx:      ctx,
+		stop:     cancel,
+	}
+
+	err := c.ping()
+	if err != nil {
+		return nil, fmt.Errorf("initial SIA ping: %w", err)
+	}
+
+	c.workers.Add(1)
+	go c.keepAlive(pingPeriod, pingError)
+
+	return c, nil
+}
+
+func (c *Client) Send(message Message) error {
+	return send(c.server, c.nextSequence(), c.identity, message)
+}
+
+func (c *Client) ping() error {
+	return c.Send(Null)
+}
+
+func (c *Client) nextSequence() uint16 {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.sequence++
+	if c.sequence > maxSequence {
+		c.sequence = 1
+	}
+	return c.sequence
+}
+
+func (c *Client) keepAlive(pingPeriod time.Duration, pingError PingError) {
+	ticker := time.NewTicker(pingPeriod)
+	defer ticker.Stop()
+	defer c.workers.Done()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := c.Send(Null)
+			if err != nil && pingError != nil {
+				pingError(err)
+			}
+		case <-c.ctx.Done():
+			return
+		}
+	}
+}
+
+func (c *Client) Close() {
+	c.stop()
+	c.workers.Wait()
+}
+
+func send(server string, sequence uint16, identity Identity, message Message) error {
 	conn, err := net.Dial("tcp", server)
 	if err != nil {
 		return fmt.Errorf("connecting to %q: %w", server, err)
 	}
 	defer conn.Close()
+
 	m := Encode(sequence, identity, message)
-	fmt.Printf("msg: %q\n", m)
 	_, err = conn.Write([]byte(m))
 	if err != nil {
 		return fmt.Errorf("sending message %q to %q: %w", m, server, err)
 	}
+
 	resp, err := bufio.NewReader(conn).ReadString(0x0D)
 	if err != nil {
 		return fmt.Errorf("reading server %q response: %w", server, err)
 	}
-	fmt.Printf("resp: %q\n", resp)
+
+	reply, id, seq, err := Parse(resp)
+	if err != nil {
+		return fmt.Errorf("parsing server %q response %q: %w", server, resp, err)
+	}
+	if id != identity {
+		return fmt.Errorf("mismatched identity %q, expected %q", id, identity)
+	}
+	if seq != sequence {
+		return fmt.Errorf("mismatched sequence %d, expected %d", seq, sequence)
+	}
+	if reply.ID() != Ack.ID() {
+		return fmt.Errorf("message not acked, got %q instead", reply.ID())
+	}
 	return nil
 }
